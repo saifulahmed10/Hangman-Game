7595420821c3a94bb948a53eacd98d2aba446d8c
Saiful Ahmed <saiful.20@hotmail.co.uk>
2023-11-14 15:29:26 +0000

    Adds two new functions check_guess and ask_for_input

--- milestone_3.py
+++ milestone_3.py
@@ -11,11 +11,30 @@ guess = input(f"Enter a single letter: ")
 # if and else statements to ensure one letter is passed through
 
 while guess:
-  if guess.isalpha() and len(guess) == 1:
+  if guess.isalpha() and len(guess) == 1 and guess.alpha() == word:
     print("Good guess!")
     guess = input(f"Enter a single letter: ")
   else:
     print("Oops! That is not a valid input.")
     guess = input(f"Enter a single letter: ")
 
+# function that checks guess
 
+def check_guess(guess):
+  guess.lower()
+  if guess.isalpha() and len(guess) == 1 and guess in word:
+    print(f"Good guess! {guess} is in the word")
+  else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
+
+#ask for input function
+
+def ask_for_input():
+  while True:
+    guess = input(f"Enter a single letter: ")
+    if guess.isalpha() and len(guess) == 1:
+      print("Good guess!")
+    else:
+      print("Oops! That is not a valid input.")
+  check_guess(guess)
+ask_for_input()
