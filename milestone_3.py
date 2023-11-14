f97d6b1278a94624f91423594843d83a02006e73
Saiful Ahmed <saiful.20@hotmail.co.uk>
2023-11-14 09:15:57 +0000

    Adds if-else statements

---
+++ milestone_3.py
@@ -0,0 +1,21 @@
+import random
+
+# Assigning the fruits to a variable
+word_list = ["apple", "banana", "grapes", "orange", "mango"]
+
+#Random word assignment
+word = random.choice(word_list)
+
+# Creating input button and assigned variable to guess
+guess = input(f"Enter a single letter: ")
+# if and else statements to ensure one letter is passed through
+
+while guess:
+  if guess.isalpha() and len(guess) == 1:
+    print("Good guess!")
+    guess = input(f"Enter a single letter: ")
+  else:
+    print("Oops! That is not a valid input.")
+    guess = input(f"Enter a single letter: ")
+
+
